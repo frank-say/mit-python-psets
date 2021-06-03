5bbd2be31d8c1bb71abfcf0a8c15dff8a5d4a9e5
frank <frank@mail.0-1-byte.com>
2021-06-03 15:57:59 +0800

    add ps0

---
+++ ps0/ps0.py
@@ -0,0 +1,14 @@
+import numpy
+
+x = input('Enter number x: ')
+x = int(x)
+
+y = input('Enter number y: ')
+y = int(y)
+
+print('x ** y =', x**y)
+
+print('log(x) =', numpy.log2(x))
+
+# import pylab
+# print('log(x) =', pylab.log2(x))
